6c0bda7407911e7faa07ec0bfff2a13fed87eb36
Code Sandbox <hello@codesandbox.io>
2022-06-27 20:00:36 +0000

    Deployment from Codesandbox.

--- ui/src/App.js
+++ ui/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home2" element={<Home2 />} />
+          <Route path="/Home2" element={<Home2 />} />
         </Routes>
       </div>
     </BrowserRouter>
