08679d82dca7fc4bf4f68fa98cb9b5a666d65216
Code Sandbox <hello@codesandbox.io>
2022-06-28 22:31:54 +0000

    Deployment from Codesandbox.

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,16 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home";
 import Home2 from "./components/Home2";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Switch>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home2" element={<Home2 />} />
-        </Switch>
+        </Routes>
       </div>
     </BrowserRouter>
   );
